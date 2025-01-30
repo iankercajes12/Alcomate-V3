f7230e36d517d3f162fa9d540fdff3bcedb0e8ac
iankercajes12 <40867950+iankercajes12@users.noreply.github.com>
2025-01-30 10:16:27 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "";
+  let ProductId = "1842265817123";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
